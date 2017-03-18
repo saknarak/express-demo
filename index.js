6e53b0b3d69fdebd255c4cd2166c1cbb04a3be8d
Somsak Sae-Lim <Somsak Sae-Lim>
2017-03-18 15:55:06 +0700

    merge with github

--- index.js
+++ index.js
@@ -13,22 +13,17 @@ app.post('/login', bodyParser.json(), (req, res) => {
     status: true,
   })
 })
-app.post('/upload', (req, res) => {
-  console.log(req.headers)
-  req.pipe(fs.createWriteStream(req.headers['x-code'] + ".jpg", {
-    flags: 'w',
-    encoding: null,
-    fd: null,
-    mode: 0666
-  }));
-})
+
 
 app.post('/upload', (req, res) => {
   let teacher = req.headers['x-teacher']
   let classroom = req.headers['x-classroom']
-  // req.pipe(fs.createWriteStream(''))
-  // save to db
-  // orm
+  // req.pipe(fs.createWriteStream(req.headers['x-code'] + ".jpg", {
+  //   flags: 'w',
+  //   encoding: null,
+  //   fd: null,
+  //   mode: 0666
+  // }));
   let code = '' + ((Math.random()*1000000000000)%1000000000)
   code = code.substr(0,3) + '-' + code.substr(3,3) + '-' + code.substr(6,3)
   // 999-999-999
